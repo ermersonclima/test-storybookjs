08b313efb4e7945185f562f8d85dc2f9b2233d1a
ermersonclima <el.erm.lima@gmail.com>
2022-04-05 13:28:46 -0300

    CREATE: styled component

---
+++ src/styled-components/buttons.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+interface ButtonsProps {
+    type: 'button'
+    value: string
+    color?: 'attention' | 'warning' | 'success'
+}
+
+export const Button01 = styled.input<ButtonsProps>`
+    background-color: ${(props) => {
+        switch (props.color) {
+            case 'attention':
+                return '#ff0'
+            case 'warning':
+                return '#f00'
+            case 'success':
+                return '#0f0'
+            default:
+                return '#fff'
+        }
+    }};
+    border: solid black 1px;
+    border-radius: 4px;
+    width: ${props => props.width || '80px'}
+    height: ${props => props.height || '20px'}   
+`;
