dcb208c2832f0aeb1f9ee4a40e1b7950c53c060a
ermersonclima <el.erm.lima@gmail.com>
2022-04-06 14:09:54 -0300

    IMPROVE: App with test function

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { ReactElement } from 'react'
 import BoxField from './components/box-field'
 import SearchBar from './components/search-bar'
 import ResultField from './components/result-field'
+import { useGetPokemonByNameQuery } from './store/apis/pokemon.api'
 
 const App = (): ReactElement => {
+  // directly param to test the hook
+  const {data} = useGetPokemonByNameQuery('ditto')
+
+  const searchPokemon = () => {
+    console.log(data)
+  }
+
   return (
     <>
       <BoxField title="Find pokemon info by name">
         <>
-          <SearchBar value="Search" />
+          <SearchBar value="Search" action={searchPokemon}/>
           <ResultField />
         </>
       </BoxField>
