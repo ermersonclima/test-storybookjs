a32b7519ae8dbae69b40a0b5365442667f690fa7
ermersonclima <el.erm.lima@gmail.com>
2022-04-06 10:51:23 -0300

    CREATE: component

---
+++ src/components/box-field.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react'
+import { Box01 } from '../styled-components/boxes'
+
+const BoxField = (): ReactElement => {
+    return (
+        <Box01>
+            <Box01>texto teste</Box01>
+        </Box01>
+    )
+}
+
+export default BoxField
