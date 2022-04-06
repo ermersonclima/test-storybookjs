d2454eee53d467f99d72ed24860ada4835463eb7
ermersonclima <el.erm.lima@gmail.com>
2022-04-06 14:21:51 -0300

    REFACTOR: format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
+export const stories = ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"];
+export const addons = [
+  "@storybook/addon-links",
+  "@storybook/addon-essentials",
+  "@storybook/addon-interactions",
+  "@storybook/preset-create-react-app",
+];
+export const framework = "@storybook/react";
+export const core = {
+  builder: "@storybook/builder-webpack5",
 };
