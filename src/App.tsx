1461598f748b7d7a874d32e50ca798c06536ed28
ermersonclima <el.erm.lima@gmail.com>
2022-04-06 11:44:21 -0300

    IMPROVE: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { ReactElement } from "react";
-import TransformCaseChip from "./components/transform-case-chip";
-import GradeAnalysisChip from "./components/grade-analysis-chip";
+import { ReactElement } from 'react'
+import BoxField from './components/box-field'
 
 const App = (): ReactElement => {
   return (
     <>
-      <GradeAnalysisChip grade={0} />
-      <TransformCaseChip label="Storybook" />
+    <BoxField />
     </>
   )
 }
