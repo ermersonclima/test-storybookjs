3e019110151745e16dd8f340c1f5336595268608
ermersonclima <el.erm.lima@gmail.com>
2022-04-06 12:54:03 -0300

    IMPROVE: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from 'react'
 import BoxField from './components/box-field'
+import SearchBar from './components/search-bar'
 
 const App = (): ReactElement => {
   return (
     <>
-    <BoxField />
+      <BoxField>
+        <SearchBar value="Search" />
+      </BoxField>
     </>
   )
 }
