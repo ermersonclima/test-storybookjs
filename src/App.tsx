b48d72e9794bb6a4da237f7c003d343a9dac1f35
ermersonclima <el.erm.lima@gmail.com>
2022-04-06 13:22:04 -0300

    IMPROVE: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ReactElement } from 'react'
 import BoxField from './components/box-field'
 import SearchBar from './components/search-bar'
+import ResultField from './components/result-field'
 
 const App = (): ReactElement => {
   return (
     <>
-      <BoxField>
-        <SearchBar value="Search" />
+      <BoxField title="Find pokemon info by name">
+        <>
+          <SearchBar value="Search" />
+          <ResultField />
+        </>
       </BoxField>
     </>
   )
