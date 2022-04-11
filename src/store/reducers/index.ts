3b43084f3d8de3651ef7ae168ccf7d7234e21c8c
ermersonclima <el.erm.lima@gmail.com>
2022-04-10 22:54:38 -0300

    IMPROVE: reducers

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { jsonPlaceholderApi } from "../apis/json-placeholder.api";
 import { pokemonApi } from "../apis/pokemon.api";
+import { pokemonName } from "../reducers/pokemon-name";
 
-const apiReducers = {
+const reducers = {
   [jsonPlaceholderApi.reducerPath]: jsonPlaceholderApi.reducer,
   [pokemonApi.reducerPath]: pokemonApi.reducer,
+  [pokemonName.name]: pokemonName.reducer
 };
 
-export default apiReducers;
+export default reducers;
